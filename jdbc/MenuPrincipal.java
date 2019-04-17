401f54b0e640138e31befd171f826f44935cc3b8
Danilo Moreira Marcomini Salve <48887666+Marcomini@users.noreply.github.com>
2019-04-17 20:25:44 -0300

    Update MenuPrincipal.java

--- jdbc/MenuPrincipal.java
+++ jdbc/MenuPrincipal.java
@@ -1,29 +1,26 @@
 package br.modelo.jdbc;
-import java.util.Scanner; 
-	 
-	 public class MenuPrincipal { 
-	 
-	 public static void main(String[] args) { 
-	 
-	 Scanner leitor = new Scanner(System.in); 
-	 Cd cd = new Cd(); 
-	 
-	 System.out.println("**********CADASTRAR CD*************"); 
-	 System.out.println("Disco: "); 
-	 cd.setNomedoCd(leitor.nextLine()); 
-	 System.out.println("Gênero: "); 
-	 cd.setGenero(leitor.nextLine()); 
-	 System.out.println("Artista: "); 
-	 cd.setArtista(leitor.nextLine()); 
-	 
-	 
-	 CdDao.cadastrar(cd); 
-	 
-	 leitor.close(); 
-	 System.out.println("Cd cadastrado!"); 
-	 } 
-	 
-	 
-	} 
 
-	
+import java.util.Scanner;
+
+public class MenuPrincipal {
+
+	public static void main(String[] args) {
+
+		Scanner leitor = new Scanner(System.in);
+		Cd cd = new Cd();
+
+		System.out.println("**********CADASTRAR CD*************");
+		System.out.println("Disco: ");
+		cd.setNomedocd(leitor.nextLine());
+		System.out.println("GÃªnero: ");
+		cd.setGenero(leitor.nextLine());
+		System.out.println("Artista: ");
+		cd.setArtista(leitor.next());
+
+		CdDao.cadastrar(cd);
+
+		leitor.close();
+		System.out.println("Cd cadastrado!");
+	}
+
+}
