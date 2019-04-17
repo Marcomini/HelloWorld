812b6841e1ff2b018128de7bf6d99cdd6e91698a
Danilo Moreira Marcomini Salve <48887666+Marcomini@users.noreply.github.com>
2019-04-17 20:56:01 -0300

    Update MostrarTudo.java

--- jdbc/MostrarTudo.java
+++ jdbc/MostrarTudo.java
@@ -1,46 +1,39 @@
 package br.modelo.jdbc;
-	 
-	 import java.sql.Connection; 
-	 import java.sql.ResultSet; 
-	 import java.sql.SQLException; 
-	 import java.util.ArrayList; 
-	 
-	 public class MostrarTudo {
 
-	 
-	
-		public static void main(String[] args) { 
-	 		ArrayList<Cd> variavel1 = new ArrayList<Cd>(); 
-	 		 Connection con = Conexao.receberConexao(); 
-			String sql = "SELECT * FROM usuario"; 
-	 		try { 
-	 		java.sql.PreparedStatement preparador = con.prepareStatement(sql); 
-			ResultSet resultado = preparador.executeQuery(); 
-	 		 
-	 		while (resultado.next()){ 
-	 			 
-	 			Cd cd = new Cd(); 
-	 			cd.setCodigo(resultado.getInt("codigo:")); 
-	 			cd.setNomedoCd(resultado.getString("Disco:")); 
-	 			cd.setGenero(resultado.getString("Genêro:")); 
-	 			cd.setArtista(resultado.getString("Artista:")); 
-	 			
-	 			 
-	 			variavel1.add(cd); 
-			} 
-	 		for(Cd variavel2:variavel1){ 
-	 		 
-	 		System.out.println("Código:" + variavel2.getCodigo()); 
-	 		System.out.println("Disco:" + variavel2.getNomedoCd()); 
-	 		System.out.println("Genêro:" + variavel2.getGenero()); 
-			System.out.println("Artista:" + variavel2.getArtista()); 
-			} 
-			} 
-			catch (SQLException e) 
-		{  
-	 			e.printStackTrace(); 
-	 		}	 
-	 	} 
-	 } 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
+public class MostrarTudo {
 
+	public static void main(String[] args) {
+		ArrayList<Cd> variavel1 = new ArrayList<Cd>();
+		Connection con = Conexao.receberConexao();
+		String sql = "SELECT * FROM usuario";
+		try {
+			java.sql.PreparedStatement preparador = con.prepareStatement(sql);
+			ResultSet resultado = preparador.executeQuery();
+
+			while (resultado.next()) {
+
+				Cd cd = new Cd();
+				cd.setCodigo(resultado.getInt("codigo:"));
+				cd.setNomedocd(resultado.getString("Disco:"));
+				cd.setGenero(resultado.getString("GenÃªro:"));
+				cd.setArtista(resultado.getString("Artista:"));
+
+				variavel1.add(cd);
+			}
+			for (Cd variavel2 : variavel1) {
+
+				System.out.println("CÃ³digo:" + variavel2.getCodigo());
+				System.out.println("Disco:" + variavel2.getNomedocd());
+				System.out.println("GenÃªro:" + variavel2.getGenero());
+				System.out.println("Artista:" + variavel2.getArtista());
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
