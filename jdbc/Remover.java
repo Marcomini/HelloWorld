799d7a4c0b418c0a42750db6b79060d8a1644d11
Danilo Moreira Marcomini Salve <48887666+Marcomini@users.noreply.github.com>
2019-04-17 21:02:28 -0300

    Update Remover.java

--- jdbc/Remover.java
+++ jdbc/Remover.java
@@ -1,32 +1,30 @@
-package br.modelo.jdbc; 
- 
- 
-import java.sql.Connection; 
-import java.sql.SQLException; 
- 
- 
- import com.mysql.jdbc.PreparedStatement; 
- 
- 
-public class Remover { 
- 
- 
- 	public static void main(String[] args) { 
- 		Connection con = Conexao.receberConexao(); 
-		String sql = "DELETE from usuario where codigo = 1"; 
-		 
-		try 
- 		{ 			PreparedStatement preparador = (PreparedStatement) con.prepareStatement(sql); 
-			preparador.execute(); 
-			preparador.close(); 
-			System.out.println("Excluido"); 
- 		} 
- 			 
- 		catch 
- 	 
- 		 (SQLException e) { 
- 			e.printStackTrace(); 
-			System.err.println("Erro ao excluir"); 
- 				 
- 		} 
- 	}  	} 
+package br.modelo.jdbc;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.mysql.jdbc.PreparedStatement;
+
+public class Remover {
+
+	public static void main(String[] args) {
+		Connection con = Conexao.receberConexao();
+		String sql = "DELETE from tabelacd where codigo = 0";
+
+		try {
+			PreparedStatement preparador = (PreparedStatement) con
+					.prepareStatement(sql);
+			preparador.execute();
+			preparador.close();
+			System.out.println("Excluido");
+		}
+
+		catch
+
+		(SQLException e) {
+			e.printStackTrace();
+			System.err.println("Erro ao excluir");
+
+		}
+	}
+}
