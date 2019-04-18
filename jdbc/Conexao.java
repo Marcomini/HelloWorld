22e06340648f108eae927ae3d4db7868e884be36
Danilo Moreira Marcomini Salve <48887666+Marcomini@users.noreply.github.com>
2019-04-17 21:03:27 -0300

    Update Conexao.java

--- jdbc/Conexao.java
+++ jdbc/Conexao.java
@@ -1,29 +1,24 @@
 package br.modelo.jdbc;
 
-import java.sql.Connection; 
-import java.sql.DriverManager; 
-import java.sql.SQLException; 
- 
-public class Conexao { 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
- 
-public static Connection receberConexao(){ 
- 	 
-	Connection con = null; 
-	try{ 
-		 
-	 
-	con = DriverManager.getConnection("jdbc:mysql://localhost:3306/tabelaCd","root",""); 
- 	System.out.println("Conectado com sucesso!"); 
- 	} 
- 	catch (SQLException e) 
-	{ 
-		e.printStackTrace(); 
-		System.err.println("Não foi possivel conectar"); 
-	} 
- 	return con; 
- } 
- 	 
+public class Conexao {
 
- 
-} 
+	public static Connection receberConexao() {
+
+		Connection con = null;
+		try {
+
+			con = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/bancocd", "root", "");
+			System.out.println("Conectado com sucesso!");
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.err.println("NÃ£o foi possivel conectar");
+		}
+		return con;
+	}
+
+}
