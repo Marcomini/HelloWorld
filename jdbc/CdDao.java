d9f48dbb4e32a0d2e52613cefa1d66a806565f0b
Danilo Moreira Marcomini Salve <48887666+Marcomini@users.noreply.github.com>
2019-04-17 21:03:00 -0300

    Update CdDao.java

--- jdbc/CdDao.java
+++ jdbc/CdDao.java
@@ -1,25 +1,25 @@
-package br.modelo.jdbc; 
+package br.modelo.jdbc;
 
-  import java.sql.Connection; 
-import java.sql.PreparedStatement; 
- import java.sql.SQLException; 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+public class CdDao {
+	public static void cadastrar(Cd VariaveldeApoio) {
+		Connection con = Conexao.receberConexao();
+		String sql = "INSERT INTO tabelacd (nomedocd, genero, artista) VALUES(?, ?, ?)";
+		try {
+			PreparedStatement preparador = con.prepareStatement(sql);
+			preparador.setString(1, VariaveldeApoio.getNomedocd());
+			preparador.setString(2, VariaveldeApoio.getGenero());
+			preparador.setString(3, VariaveldeApoio.getArtista());
 
-public class CdDao { 
- public static void cadastrar(Cd VariaveldeApoio){ 
-Connection con = Conexao.receberConexao(); 
-String sql = "INSERT INTO usuario(nome, genero, artista) VALUES(?, ?, ?)"; 
-try { 
- PreparedStatement preparador = con.prepareStatement(sql); 
- preparador.setString(1, VariaveldeApoio.getNomedoCd()); 
- preparador.setString(2, VariaveldeApoio.getGenero()); 
- preparador.setString(3, VariaveldeApoio.getArtista()); 
- 
- preparador.execute(); 
- preparador.close(); 
-System.out.println("Adicionado com Sucesso!"); 
- } catch (SQLException e) { 
- e.printStackTrace(); 
- System.err.println("Não Foi Possivel Adicionar..."); 
-} 
- }} 
+			preparador.execute();
+			preparador.close();
+			System.out.println("Adicionado com Sucesso!");
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.err.println("NÃ£o Foi Possivel Adicionar...");
+		}
+	}
+}
