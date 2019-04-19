9a6ab5ea3014645c271adc4b7571f099d953cee8
Danilo Moreira Marcomini Salve <48887666+Marcomini@users.noreply.github.com>
2019-04-18 21:09:22 -0300

    Update MenuPrincipal.java

--- jdbc/MenuPrincipal.java
+++ jdbc/MenuPrincipal.java
@@ -1,6 +1,12 @@
-package br.modelo.jdbc;
+package principal;
 
 import java.util.Scanner;
+
+import br.modelo.jdbc.Alterar;
+import br.modelo.jdbc.Cd;
+import br.modelo.jdbc.CdDao;
+import br.modelo.jdbc.MostrarTudo;
+import br.modelo.jdbc.Remover;
 
 public class MenuPrincipal {
 
@@ -12,9 +18,9 @@ public class MenuPrincipal {
 				int continuar=1;
 				do{
 					System.out.println("MENU PRINCIPAL");
-					System.out.println("1 - Cadastrar Cliente");
-					System.out.println("2 - Mostrar Todos Cliente");
-					System.out.println("3 - Selecionar Cliente");
+					System.out.println("1 - Cadastrar");
+					System.out.println("2 - Alterar  ");
+					System.out.println("3 - Mostrar");
 					System.out.println("4 - Excluir Cliente");
 					System.out.println("5 - Sair");
 					System.out.print("Digite a opção desejada: ");
@@ -37,24 +43,38 @@ public class MenuPrincipal {
 							System.out.println("Cd cadastrado!");
 							break;
 						case 2:
-							System.out.println("*****************");
-							System.out.println("MOSTRAR TODOS");
-							System.out.println("*****************");
+							
+							System.out.println("ALTERAR CD");
+							System.out.println("Código:");
+							cd.setCodigo (leitor.nextInt());
+							System.out.println("Disco: ");
+							cd.setNomedocd(leitor.next());
+							System.out.println("Gênero: ");
+							cd.setGenero(leitor.next());
+							System.out.println("Artista: ");
+							cd.setArtista(leitor.next());
+							
+							Alterar.alterando(cd);
+							
+							leitor.close();
+							System.out.println("Alterado com sucesso!");
 							break;
 						case 3:
-							System.out.println("*****************");
-							System.out.println("SELECIONAR CLIENTE");
-							System.out.println("*****************");
+							System.out.println("Mostrar CD");
+								MostrarTudo.mostrando();
+								
 							break;
 						case 4:
-							System.out.println("*****************");
-							System.out.println("EXCLUIR CLIENTE");
-							System.out.println("*****************");
+							System.out.println("EXCLUIR CD");
+														
+							System.out.println("Código:");
+							int codigo = leitor.nextInt();
+							
+							Remover.removendo(codigo);
+							
 							break;
 						case 5:
-							System.out.println("*****************");
 							System.out.println("PROGRAMA FINALIZADO!");
-							System.out.println("*****************");
 							System.exit(1);
 					}
 					
